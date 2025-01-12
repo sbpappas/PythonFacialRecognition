fcfc75253bb0e67391c9df3a4026d0f52d44e06a
Samuel Pappas <spappas@trinity.edu>
2025-01-11 20:39:47 -0600

    starting off tonight

---
+++ facerecognition.py
@@ -0,0 +1,22 @@
+import cv2
+#import face_recognition
+#import dlib
+import os, sys
+import numpy as np
+import threading
+
+from deepface import DeepFace
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) #picks the first camera
+cap.set
+cap.set
+
+counter = 0
+
+face_match = False
+
+referenceImage = cv2.imread("reference.jpeg")
+While 
+
+print("hi")
+print(cv2.__version__)
